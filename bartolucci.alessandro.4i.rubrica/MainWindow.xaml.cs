2fb30d4b6b43a91403b2a7e6c3ed8b9b31dadf8b
Alessandro Bartolucci <127590077+ale02082000@users.noreply.github.com>
2024-02-09 11:57:45 +0100

    Update MainWindow.xaml.cs

--- bartolucci.alessandro.4i.rubrica/MainWindow.xaml.cs
+++ bartolucci.alessandro.4i.rubrica/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace bartolucci.alessandro._4i.rubrica
-{ 
- 
+{
+
     public partial class MainWindow : Window
     {
         List<Persona> Persone = new List<Persona>();
@@ -21,50 +21,53 @@ namespace bartolucci.alessandro._4i.rubrica
             try
             {
                 StreamReader finPersone = new StreamReader("persone.csv");
-                finPersone.ReadLine(); 
+                finPersone.ReadLine();
                 while (!finPersone.EndOfStream)
                 {
                     string rigaPersona = finPersone.ReadLine();
                     Persona persona = new Persona(rigaPersona);
                     Persone.Add(persona);
                 }
-                dgPersone.ItemsSource = Persone; 
+                dgPersone.ItemsSource = Persone;
 
                 statusBarText.Text = $"Ho letto dal file {Persone.Count} persone";
                 finPersone.Close();
-                
-              
+
+
                 StreamReader finContatti = new StreamReader("contatti.csv");
-                finContatti.ReadLine(); 
+                finContatti.ReadLine();
                 while (!finContatti.EndOfStream)
                 {
                     string rigaContatto = finContatti.ReadLine();
                     Contatto contatto = new Contatto(rigaContatto);
                     Contatti.Add(contatto);
-                } 
-               // dgContatti.ItemsSource = Contatti;
+                }
+                // dgContatti.ItemsSource = Contatti;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Errore: " + ex.Message);
             }
-        } 
+        }
         private void dgDati_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Persona p = e.AddedItems[0] as Persona;
-           if (p != null)
+            if (p != null)
             {
-                statusbar.Text = $" Hai selezionato {p.Nome} {p.Cognome}";
+                statusBarText.Text = $" Hai selezionato {p.Nome} {p.Cognome}";
 
-            List<Contatto> contattiFiltrati = new List<Contatto>();
-            foreach(var item in Contatti)
-                if(item.IdPersona == p.IdPersona)
-                    contattiFiltrati.Add(item);
+                List<Contatto> contattiFiltrati = new List<Contatto>();
+                foreach (var item in Contatti)
+                    if (item.IdPersona == p.IdPersona)
+                        contattiFiltrati.Add(item);
 
-            dgContatti.ItemsSource = contattiFiltrati;
+                dgContatti.ItemsSource = contattiFiltrati;
 
+            }
+         
         }
-             private void dgContatti_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void dgContatti_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
